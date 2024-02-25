dffffa00967198c5cecd9b787ec44f31bbe16a8a
Soufiane Fathaoui <soufianenakata7@gmail.com>
2024-02-25 18:56:37 +0100

    Add,Delete,Updtate, is done

--- server/routes/notes.js
+++ server/routes/notes.js
@@ -1,7 +1,6 @@
 import express from "express";
 const router =  express.Router()
 import {getNotes , getSingalNotes , createNotes, updateNote , deletNote} from '../controllers/notes.js';
-import { get } from "mongoose";
 
 router.route("/").get(getNotes).post(createNotes)
 router.route("/:id").get(getSingalNotes).put(updateNote).delete(deletNote)
--- server/server.js
+++ server/server.js
@@ -1,14 +1,15 @@
+// The required 3line in this file
 import express from "express";
+const app = express()
+app.use(express.json())
+const _port = 3000
+
 import notes from "./routes/notes.js";
 import mongoose from "mongoose";
 import morgan  from 'morgan';
 
-const app = express()
-app.use(express.json())
-const _port = 3000
 app.use(morgan("dev"));
 app.use("/api/v1/notes",notes)
-
 
 mongoose.connect(`mongodb+srv://soufianenakata7:9ZNSSHCN65QekB2p@cluster0.myo2rpg.mongodb.net/All-data?retryWrites=true&w=majority&appName=Cluster0`)
   .then(() => {
